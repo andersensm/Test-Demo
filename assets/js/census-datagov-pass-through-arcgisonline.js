a9b629382e4e5051982754f4a869e45fa81d8eff
Sean Andersen <smandersen13@gmail.com>
2018-09-15 13:07:23 -0400

    working main three layers

--- assets/js/census-datagov-pass-through-arcgisonline.js
+++ assets/js/census-datagov-pass-through-arcgisonline.js
@@ -2,31 +2,31 @@ var map = L.map('map').setView([38.9072, -77.0369], 10);
 L.esri.basemapLayer("Streets").addTo(map);
 
 var icons = {
-  5: L.ExtraMarkers.icon({
+  best: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'green-dark',
     shape: 'circle',
     prefix: 'fa'
   }),
-  4: L.ExtraMarkers.icon({
+  secondBest: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'green-light',
     shape: 'circle',
     prefix: 'fa'
   }),
-  3: L.ExtraMarkers.icon({
+  mediocre: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'yellow',
     shape: 'circle',
     prefix: 'fa'
   }),
-  2: L.ExtraMarkers.icon({
+  secondWorste: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'orange',
     shape: 'circle',
     prefix: 'fa'
   }),
-  1: L.ExtraMarkers.icon({
+  worste: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'red',
     shape: 'circle',
@@ -41,63 +41,100 @@ var virginiaCounties = L.esri.featureLayer({
 
 var cdcCounties = L.esri.featureLayer({
   url: "https://services3.arcgis.com/ZvidGQkLaDJxRSJ2/arcgis/rest/services/Socioeconomic_2014_Counties/FeatureServer/0",
-  style: function (feature) {
-  if (feature.properties.RPL_THEMES >= '0.80') {
-    return {color: '#8B0000', weight: 2 };
-  } else if ((feature.properties.RPL_THEMES < '0.80') & (feature.properties.RPL_THEMES >= '0.60')) {
-    return { color: 'orange', weight: 2 };
-  } else if ((feature.properties.RPL_THEMES < '0.60') & (feature.properties.RPL_THEMES >= '0.40')) {
-    return { color: 'yellow', weight: 2 };
-  } else if ((feature.properties.RPL_THEMES < '0.40') & (feature.properties.RPL_THEMES >= '0.20')) {
-    return { color: '#32CD32', weight: 1 };
-  } else {
-    return { color: '#006400', weight: 2 };
+  style: function(feature) {
+    if (feature.properties.RPL_THEMES >= '0.80') {
+      return {
+        color: '#8B0000',
+        weight: 2
+      };
+    } else if ((feature.properties.RPL_THEMES < '0.80') & (feature.properties.RPL_THEMES >= '0.60')) {
+      return {
+        color: 'orange',
+        weight: 2
+      };
+    } else if ((feature.properties.RPL_THEMES < '0.60') & (feature.properties.RPL_THEMES >= '0.40')) {
+      return {
+        color: 'yellow',
+        weight: 2
+      };
+    } else if ((feature.properties.RPL_THEMES < '0.40') & (feature.properties.RPL_THEMES >= '0.20')) {
+      return {
+        color: '#32CD32',
+        weight: 1
+      };
+    } else {
+      return {
+        color: '#006400',
+        weight: 2
+      };
+    }
   }
-}
 })
 
 ///Medicare & Medicaid Services Compare Information///////////
 var hospitalRatings = L.esri.featureLayer({
   url: "https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/Hospital_General_Information/FeatureServer/0",
-  pointToLayer: function (feature, latlng) {
-    return L.marker(latlng, {
-      icon: icons[feature.properties.Hospital_overall_rating]
-    });
-  },
+  pointToLayer: function(feature, latlng) {
+    if (feature.properties.Hospital_overall_rating === '5') {
+      return L.marker(latlng, {
+        icon: icons.best
+      })
+    } else if (feature.properties.Hospital_overall_rating === '4') {
+      return L.marker(latlng, {
+        icon: icons.secondBest
+      })
+    } else if (feature.properties.Hospital_overall_rating === '3') {
+      return L.marker(latlng, {
+        icon: icons.mediocre
+      })
+    } else if (feature.properties.Hospital_overall_rating === '2') {
+      return L.marker(latlng, {
+        icon: icons.secondWorste
+      })
+    } else {
+      return L.marker(latlng, {
+        icon: icons.worste
+      })
+    }
+  }
 })
 
 $("#censusDataGov").on("click", function() {
-          if (this.checked === true) {
-                        //START/////Downloaded from Census.gov, unchanged//
-                        virginiaCounties.addTo(map);
-                        //
-                        var popupTemplate = "<h3>{NAMELSAD}</h3><br><h4>{ALAND} Land Area<h4><br><h5>Lat: {INTPTLAT}</h5><br><h5>Lng: {INTPTLON}</h5>";
-                        //
-                        virginiaCounties.bindPopup(function(e) {
-                          return L.Util.template(popupTemplate, e.feature.properties)
-                        });
-                        //END/////Downloaded from Census.gov, unchanged//
-          } else {
-            map.removeLayer(virginiaCounties)
-          }
+  if (this.checked === true) {
+    //START/////Downloaded from Census.gov, unchanged//
+    virginiaCounties.addTo(map);
+    //
+    var popupTemplate = "<h3>{NAMELSAD}</h3><br><h4>{ALAND} Land Area<h4><br><h5>Lat: {INTPTLAT}</h5><br><h5>Lng: {INTPTLON}</h5>";
+    //
+    virginiaCounties.bindPopup(function(e) {
+      return L.Util.template(popupTemplate, e.feature.properties)
+    });
+    //END/////Downloaded from Census.gov, unchanged//
+  } else {
+    map.removeLayer(virginiaCounties)
+  }
 })
 
 $("#cdc").on("click", function() {
-        if (this.checked === true) {
-                        cdcCounties.addTo(map);
-                        var popupTemplate = "<h3>{LOCATION}</h3><br><h4>Socio Economic Vulnerability Index-CDC</h4><br><h5>Vulnerability Rating(1-High, 0-Low): <strong>{RPL_THEMES}<strong></h5>";
-                        cdcCounties.bindPopup(function(e) {
-                          return L.Util.template(popupTemplate, e.feature.properties)
-                        });
-          } else {
-            map.removeLayer(cdcCounties)
-          }
+  if (this.checked === true) {
+    cdcCounties.addTo(map);
+    var popupTemplate = "<h3>{LOCATION}</h3><br><h4>Socio Economic Vulnerability Index-CDC</h4><br><h5>Vulnerability Rating(1-High, 0-Low): <strong>{RPL_THEMES}<strong></h5>";
+    cdcCounties.bindPopup(function(e) {
+      return L.Util.template(popupTemplate, e.feature.properties)
+    });
+  } else {
+    map.removeLayer(cdcCounties)
+  }
 })
 
 $("#medicareMedicaid").on("click", function() {
-        if (this.checked === true) {
-                        hospitalRatings.addTo(map);
-          } else {
-            map.removeLayer(hospitalRatings)
-          }
+  if (this.checked === true) {
+    hospitalRatings.addTo(map);
+    var popupTemplate = "<h3>{Hospital_Name}</h3><br><h4>Medicare & Medicaid Certified Hospital Ratings</h4><br><h5>Hospital Rating(5-Best, 1-Worste): <strong>{Hospital_overall_rating}<strong></h5>";
+    hospitalRatings.bindPopup(function(e) {
+      return L.Util.template(popupTemplate, e.feature.properties)
+    });
+  } else {
+    map.removeLayer(hospitalRatings)
+  }
 })
