a571853822a65c85249ca15a5a7bdc98dc088ac7
Sean Andersen <smandersen13@gmail.com>
2018-09-15 16:31:30 -0400

    reformated Nav bar

--- assets/js/census-datagov-pass-through-arcgisonline.js
+++ assets/js/census-datagov-pass-through-arcgisonline.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([38.9072, -77.0369], 10);
 L.esri.basemapLayer("Streets").addTo(map);
 
-var icons = {
+var iconsHospitalRankings = {
   best: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'green-dark',
@@ -29,6 +29,45 @@ var icons = {
   worste: L.ExtraMarkers.icon({
     icon: 'fa-hospital-symbol',
     markerColor: 'red',
+    shape: 'circle',
+    prefix: 'fa'
+  })
+};
+
+var iconsMedicareSpending = {
+  best: L.ExtraMarkers.icon({
+    icon: 'fa-usd-circle',
+    markerColor: 'green-dark',
+    shape: 'circle',
+    prefix: 'fa'
+  }),
+  secondBest: L.ExtraMarkers.icon({
+    icon: 'fa-usd-circle',
+    markerColor: 'green-light',
+    shape: 'circle',
+    prefix: 'fa'
+  }),
+  mediocre: L.ExtraMarkers.icon({
+    icon: 'fa-usd-circle',
+    markerColor: 'yellow',
+    shape: 'circle',
+    prefix: 'fa'
+  }),
+  secondWorste: L.ExtraMarkers.icon({
+    icon: 'fa-usd-circle',
+    markerColor: 'orange',
+    shape: 'circle',
+    prefix: 'fa'
+  }),
+  worste: L.ExtraMarkers.icon({
+    icon: 'fa-usd-circle',
+    markerColor: 'red',
+    shape: 'circle',
+    prefix: 'fa'
+  }),
+  notAvailable: L.ExtraMarkers.icon({
+    icon: 'fa-usd-circle',
+    markerColor: 'black',
     shape: 'circle',
     prefix: 'fa'
   })
@@ -77,23 +116,52 @@ var hospitalRatings = L.esri.featureLayer({
   pointToLayer: function(feature, latlng) {
     if (feature.properties.Hospital_overall_rating === '5') {
       return L.marker(latlng, {
-        icon: icons.best
+        icon: iconsHospitalRankings.best
       })
     } else if (feature.properties.Hospital_overall_rating === '4') {
       return L.marker(latlng, {
-        icon: icons.secondBest
+        icon: iconsHospitalRankings.secondBest
       })
     } else if (feature.properties.Hospital_overall_rating === '3') {
       return L.marker(latlng, {
-        icon: icons.mediocre
+        icon: iconsHospitalRankings.mediocre
       })
     } else if (feature.properties.Hospital_overall_rating === '2') {
       return L.marker(latlng, {
-        icon: icons.secondWorste
+        icon: iconsHospitalRankings.secondWorste
       })
     } else {
       return L.marker(latlng, {
-        icon: icons.worste
+        icon: iconsHospitalRankings.worste
+      })
+    }
+  }
+})
+
+
+///Medicare Hospital Spending Per Patient///////////
+var medicareSpending = L.esri.featureLayer({
+  url: "https://services1.arcgis.com/4yjifSiIG17X0gW4/arcgis/rest/services/Medicare_Hospital_Spending_per_Patient/FeatureServer/0",
+  pointToLayer: function(feature, latlng) {
+    if (feature.properties.Score === 'Null') {
+      return L.marker(latlng, {
+        icon: iconsMedicareSpending.notAvailable
+      })
+    } else if ((feature.properties.Score >= '1.2') & (feature.properties.Score <= '2.63'))  {
+      return L.marker(latlng, {
+        icon: iconsMedicareSpending.worste
+      })
+    } else if ((feature.properties.Score < '1.2') & (feature.properties.Score >= '1.0')) {
+      return L.marker(latlng, {
+        icon: iconsMedicareSpending.secondWorste
+      })
+    } else if ((feature.properties.Score < '1.0') & (feature.properties.Score >= '0.8')) {
+      return L.marker(latlng, {
+        icon: iconsMedicareSpending.secondBest
+      })
+    } else if ((feature.properties.Score < '0.8') & (feature.properties.Score >= '0.1')) {
+      return L.marker(latlng, {
+        icon: iconsMedicareSpending.best
       })
     }
   }
@@ -136,5 +204,17 @@ $("#medicareMedicaid").on("click", function() {
     });
   } else {
     map.removeLayer(hospitalRatings)
+  }
+})
+
+$("#medicareSpending").on("click", function() {
+  if (this.checked === true) {
+    medicareSpending.addTo(map);
+    var popupTemplate = "<h3>{Hospital_Name}</h3><br><h4>Medicare Spending Per Patient 2016</h4><br><h5>Medicare Spending Per Patient</h5><br>(1.8-High Above National Average(Bad), 1-National Average, 0-Below National Average(Good)): <h5>Rating: <strong>{Score}<strong></h5>";
+    medicareSpending.bindPopup(function(e) {
+      return L.Util.template(popupTemplate, e.feature.properties)
+    });
+  } else {
+    map.removeLayer(medicareSpending)
   }
 })
